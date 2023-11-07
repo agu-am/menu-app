c61c63bc0cd05f3dc937aa5fac616fe43a9bfdd7
agu-am <agustin.cs96@gmail.com>
2023-11-07 17:34:31 -0300

    first commit

---
+++ pages/admin.js
@@ -0,0 +1,24 @@
+import useSWR from "swr"
+import axios from "axios"
+import AdminLayout from "../layout/AdminLayout"
+import Orden from "../components/Oreden"
+
+export default function Admin() {
+    const fetcher = () => axios('/api/ordenes').then(datos => datos.data)
+
+    const { data, error, isLoading } = useSWR('/api/ordenes', fetcher, { refreshInterval: 50 })
+
+    return (
+        <AdminLayout pagina={'Admin'}>
+            <h1 className="text-4xl font-black">Panel de administración</h1>
+            <p className="text-2xl my-10"> Administra las ordenes</p>
+
+            {data && data.length ? data.map( orden => 
+                <Orden 
+                    key={orden.id}
+                    orden={orden}
+                />
+                ) : <p>No hay ordenes pendientes</p>}
+        </AdminLayout>
+    )
+}
